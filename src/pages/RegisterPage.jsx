031c4397f0acd167761e651b5ab3e3c576948b08
MEHAKABDULLAH <mehakabdullah170@gmail.com>
2025-02-06 09:53:16 +0500

    remove

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -4,7 +4,7 @@ import RegisterForm from '../component/RegisterForm';
 import { Link } from 'react-router-dom';
 import { auth, createUserWithEmailAndPassword } from '../Authentication/firebase';
 import { Typography, Layout } from 'antd';
-import { Link,useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const { Title, Paragraph } = Typography;
 const { Content } = Layout;
